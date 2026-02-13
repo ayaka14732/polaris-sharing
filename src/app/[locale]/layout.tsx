b937b90b72fbe43802604d97fdeebedae7599c78
Ayaka <ayaka@mail.shn.hk>
2026-02-13 18:12:38 +0100

    Add metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,21 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Script from "next/script";
 import "../globals.css";
 
 const locales = ["zh-CN", "zh-TW", "en"];
+
+export const metadata: Metadata = {
+  title: "明日北极星个人分享",
+  keywords: ["个人网站", "网页小工具", "单位换算", "农历转换", "行程记录"],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default async function LocaleLayout({
   children,
